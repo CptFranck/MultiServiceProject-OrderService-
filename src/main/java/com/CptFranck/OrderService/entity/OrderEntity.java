81b7f3f5c6417e4235fc733e2cf1ce80d6dd54fd
CptFranck <francois.caillet71@gmail.com>
2025-10-06 17:08:02 +0200

    add OrderEntity entity

---
+++ src/main/java/com/CptFranck/OrderService/entity/OrderEntity.java
@@ -0,0 +1,37 @@
+package com.CptFranck.OrderService.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "order")
+public class OrderEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "total")
+    private BigDecimal totalPrice;
+
+    @Column(name="quantity")
+    private Long ticketCount;
+
+    @CreationTimestamp
+    @Column(name = "placed_at", updatable = false, nullable = false)
+    private LocalDateTime placedAt;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "event_id")
+    private Long eventId;
+}
